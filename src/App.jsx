e4bf19455a19255d1aea63c1c9de6dcc71d32074
mr-dev360 <maheshdgaikwa18@gmail.com>
2024-10-05 22:46:57 +0530

    Added dynamic rendering for animal details using map() in App component

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+function App() {
+  let animalDetails = [
+    {
+      name: 'dog',
+      breed: 'Labrador',
+      age: 1.5,
+    },
+    {
+      name: 'tiger',
+      breed: 'indian tiger',
+      age: 3,
+    },
+    {
+      name: 'elephant',
+      breed: 'African elephant',
+      age: 10,
+    },
+    {
+      name: 'lion',
+      breed: 'African lion',
+      age: 4,
+    }
+  ]
+  let count = 0;
+
+  let x = animalDetails.map((item,index)=>{
+            return (
+              <div key = {index}>
+                <h1>name : {item.name}</h1>
+                <h1>native : {item.breed}</h1>
+                <h1>status : {item.age}</h1>
+              </div>
+            )
+  })
+
+  return (
+    <>  {/* { fragment ka use karke kisi bhi elemt ko wrap kr sakte hai wo frontend mein dikhega nai } */}
+    <h1 className='bg-orange-400'>{count}</h1>
+    <h1>{x}</h1>
+       {/* <h1 >this is react vite boilerplate {import.meta.env.VITE_API}</h1> */}
+    </>
+  )
+}
+
+export default App
