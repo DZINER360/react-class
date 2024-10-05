c9be69c6b927cfa9d283dea5fedca1793f4c4731
mr-dev360 <maheshdgaikwa18@gmail.com>
2024-10-05 22:58:13 +0530

    React Event Handling with Static, Dynmic Fun Calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,19 @@
 import { useState } from 'react'
-// class 1 - 3
+// class 3
+
+  let info = () =>{
+    console.log("hello world")
+  }
+  let advanced = (data) =>{
+    console.log(data)
+  }
 function App() {
-  let animalDetails = [
-    {
-      name: 'dog',
-      breed: 'Labrador',
-      age: 1.5,
-    },
-    {
-      name: 'tiger',
-      breed: 'indian tiger',
-      age: 3,
-    },
-    {
-      name: 'elephant',
-      breed: 'African elephant',
-      age: 10,
-    },
-    {
-      name: 'lion',
-      breed: 'African lion',
-      age: 4,
-    }
-  ]
-  let count = 0;
-
-  let x = animalDetails.map((item,index)=>{
-            return (
-              <div key = {index}>
-                <h1>name : {item.name}</h1>
-                <h1>native : {item.breed}</h1>
-                <h1>status : {item.age}</h1>
-              </div>
-            )
-  })
-
-  return (
-    <>  {/* { fragment ka use karke kisi bhi elemt ko wrap kr sakte hai wo frontend mein dikhega nai } */}
-    <h1 className='bg-orange-400'>{count}</h1>
-    <h1>{x}</h1>
-       {/* <h1 >this is react vite boilerplate {import.meta.env.VITE_API}</h1> */}
-    </>
-  )
+    return (
+        <div>
+          <button onClick={info} className='bg-pink-300 rounded py-2 px-5' >hello ji</button>
+          <button onClick={()=>advanced("react worldd")} className='bg-orange-600 rounded py-2 px-5' >hello to you</button>
+        </div>
+      )
 }
 
 export default App
